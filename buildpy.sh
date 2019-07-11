66ab902905687681d37e859ad6f24c9f1987e0a4
Matthew J. [Jacobian] McKay <dday87@uvic.ca>
2019-07-10 17:25:37 -0700

    Using LD_LIBRARY_PATH and make destdir=~/ install

--- buildpy.sh
+++ buildpy.sh
@@ -1,9 +1,7 @@
 !#/bin/bash
+#INSTRUCTIONS
+#1 build c-blosc 1.14.2 from source (USED BY python-blosc 1.5.1 build) 
 echo "Please be sure to chmod u+x buildpy.sh prior to use!"
-mkdir TopLevelbuild
-TLB="TopLevelBuild"
-export TLDB
-cd TLDB
 wget "https://github.com/Blosc/c-blosc/archive/v1.14.2.tar.gz"
 sha256sum v1.14.2.tar.gz > v1.14.2.tar.gz.sha256
 sha256sum -c v1.14.2.tar.gz.sha256
@@ -17,13 +15,41 @@ cd build
 mkdir ../buildlogs
 cmake .. | tee ../buildlogs/cmakeconf.log.txt
 make -j $(nproc) | tee ../buildlogs/build.log.txt
+make install DESTDIR=~/ -j $(nproc)
 cd blosc
-CBLOSCDIR=$(pwd)
-export CBLOSCDIR
-echo $CBLOSCDIR
-#INSTRUCTIONS
-#1 build c-blosc 1.14.2 from source (USED BY python-blosc 1.5.1 build) 
+CBLOSCLIBDIR=$(pwd)
+export CBLOSCLIBDIR
+echo $CBLOSCLIBDIR
 #2 build OpenSSL 1.0.2o from source (USED BY Python 3.5.4 build)
+cd ../../../
+wget https://www.openssl.org/source/old/1.0.2/openssl-1.0.2o.tar.gz
+wget https://www.openssl.org/source/old/1.0.2/openssl-1.0.2o.tar.gz.sha256
+wget https://www.openssl.org/source/old/1.0.2/openssl-1.0.2o.tar.gz.sha1
+wget https://www.openssl.org/source/old/1.0.2/openssl-1.0.2o.tar.gz.asc
+#sha256sum openssl-1.0.2o.tar.gz > openssl-1.0.2o.tar.gz.sha256
+sha256sum -c openssl-1.0.2o.tar.gz.sha256
+#sha1sum openssl-1.0.2o.tar.gz > openssl-1.0.2o.tar.gz.sha1
+sha1sum -c openssl-1.0.2o.tar.gz.sha1
+gpgv openssl-1.0.2o.tar.gz.asc openssl-1.0.2o.tar.gz
+tar -xvf openssl-1.0.2o.tar.gz
+rm openssl-1.0.2o.tar.gz
+rm openssl-1.0.2o.tar.gz.sha256
+rm openssl-1.0.2o.tar.gz.sha1
+rm openssl-1.0.2o.tar.gz.asc
+cd openssl-1.0.2o
+mkdir buildlogs
+./config | tee buildlogs/config.log.txt
+make -j $(nproc) | tee buildlogs/opensslbuild.log.txt
+cd include/openssl
+OPENSSL_INCLUDES=$(pwd)
+export OPENSSL_INCLUDES
+cd ../..
+#If OPENSSLv < 1.1.x
+make INSTALL_PREFIX=~/ install
+#If OPENSSLv > 1.1.x
+# make install DESTDIR=~/ 
+export PATH=~/usr/local/ssl/bin:$PATH
+export LD_LIBRARY_PATH=~/usr/local/ssl/lib:$LD_LIBRARY_PATH
 #3 build python-blosc 1.5.1 python package from source 
 #4 build Python 3.5.4 from source
 #5 extend interface to automate other builds
--- instructions.md
+++ instructions.md
@@ -4,15 +4,28 @@ INSTRUCTIONS
 ------------
 1. Change permissions of buildpy.sh prior to use ([user@host]$chmod u+x buildpy.sh)
 2. build c-blosc 1.14.2 from source (USED BY python-blosc 1.5.1 build) 
-   * Find source URL "https://github.com/Blosc/c-blosc/archive/v1.14.2.tar.gz"
-   * Create a working directory ([user@host]$ mkdir autobuildpy; cd autobuildpy)
-   * Download source file using wget ([user@host]$ wget $CBLOSC_URL
-   * Verify sha256sum checksums
+   * Find source URL "CBLOSC_URL=https://github.com/Blosc/c-blosc/archive/v1.14.2.tar.gz"
+   * Create a working directory ([user@host]$ mkdir TopLevelbuild; cd TopLevelbuild)
+   * Download source file using wget ([user@host]$ wget $CBLOSC_URL)
+   * Verify sha256sum checksums sha256sum -c $CBLOSC.sha256
    * (Optional if no Signatures available) Verify GPG sig (using gpgv2 (Setup alias [User@host]$ alias gpgv=gpgv2)
    * untar archive [user@host]$ tar -xvf $CBLOSCARCHIVE
-   * Clean up directory
-   * 
+   * Clean up directory rm *tar.gz; rm *.sha256
+   * Create a buildlog directory [user@host]$ mkdir buildlogs
+   * Use cmake
+   * make -j (Number of procs) e.g. $(nproc)
+   * Set an environment variable to locate c-blosc shared objects
 3. build OpenSSL 1.0.2o from source (USED BY Python 3.5.4 build)
+   * Find source URL "OPENSSL1-0-2o_URL=https://www.openssl.org/source/old/1.0.2/openssl-1.0.2o.tar.gz"
+   * Download source file using wget ([user@host]$ wget $OPENSSL1-0-2o_URL
+   * Verify sha256sum checksum1
+   * Verify sha1sum checksum
+   * (Optional if no Signatures available) Verify GPG sig ([User@host]$gpgv $OPENSSL1-0-2o.tar.gz.asc $OPENSSL1-0-2o.tar.gz)
+   * untar archive [user@host]$ tar -xvf $OPENSSL1-0-2oARCHIVE
+   * Clean up directory
+   * config for host
+   * make 
+   * Set an environment variable to locate openssl binaries and shared objects
 4. build python-blosc 1.5.1 python package from source 
 5. build Python 3.5.4 from source
 6. extend interface to automate other builds
